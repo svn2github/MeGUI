6d53b29cb51764dd50e8e19f73c7e57f046b3ee0
berrinam <berrinam@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2006-08-19 04:29:32 +0000

    Updated compile.bat to work with folders.
    git-svn-id: http://svn.code.sf.net/p/megui/code@40 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -1,28 +1,7 @@
-@ECHO OFF
-IF "%1"=="full" GOTO Full
-IF "%1"=="" GOTO FULL
-GOTO Syntax
+%windir%\Microsoft.NET\Framework\v2.0.50727\csc /platform:x86 /res:App.ico /res:Changelog.txt,MeGUI.Changelog.txt /res:pause.ico /res:play.ico /r:MessageBoxExLib.dll /r:Microsoft.VisualBasic.dll /r:ICSharpCode.SharpZiplib.dll /r:MediaInfoWrapper.dll /target:winexe /out:megui.exe /win32icon:app.ico /unsafe+ /recurse:*.cs /d:CSC /o /warn:0
 
-:Full
-%windir%\Microsoft.NET\Framework\v2.0.50727\csc /platform:x86 /res:App.ico /res:Changelog.txt,MeGUI.Changelog.txt /res:pause.ico /res:play.ico /r:MessageBoxExLib.dll /r:Microsoft.VisualBasic.dll /r:ICSharpCode.SharpZiplib.dll /r:MediaInfoWrapper.dll /target:winexe /out:megui.exe /win32icon:app.ico /unsafe+ *.cs /d:CSC /o
-rem %windir%\Microsoft.NET\Framework\v2.0.50727\csc /platform:x86 /target:exe /out:neroraw.exe /unsafe+ neroraw\*.cs /d:CSC /o
-%windir%\Microsoft.NET\Framework\v2.0.50727\csc /platform:x86 /target:exe /out:updatecopier.exe updatecopier\*.cs /d:CSC /o
-IF "%1"=="" GOTO Pausing
-GOTO End
-
-:Syntax
-ECHO.
-ECHO MeGUI compilation script 1.2
-ECHO.
-ECHO Usage: compile.bat [mode]
-ECHO.
-ECHO WHERE: [mode]   full     : full mode
-ECHO.
-GOTO End
-
-:Pausing
 PAUSE
-:End
+
 rem make dist with everything
 md Dist\BigDist
 copy *.exe .\Dist\BigDist
@@ -51,3 +30,5 @@ md Dist\updatecopier
 copy updatecopier.exe .\Dist\updatecopier
 md Dist\Data
 copy .\Data\*.xml .\Dist\Data
+
+PAUSE
