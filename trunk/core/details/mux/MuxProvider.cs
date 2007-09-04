55840f0c69792c90102657218da2eb19e1ae0f63
Kurtnoise13 <Kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-09-04 11:42:29 +0000

    add MP4 Container support for the XviD compressor.
    git-svn-id: http://svn.code.sf.net/p/megui/code@142 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/core/details/mux/MuxProvider.cs
+++ trunk/core/details/mux/MuxProvider.cs
@@ -1009,6 +1009,7 @@ namespace MeGUI
             supportedCodecs.Add(VideoCodec.ASP);
             supportedTypes.Add(VideoType.AVI);
             supportedTypes.Add(VideoType.MKV);
+            supportedTypes.Add(VideoType.MP4);
             supportedTypes.Add(VideoType.RAWASP);
             supportedEncoderTypes.Add(VideoEncoderType.XVID);
         }
