5a2bfa98336c1d0bdaa01f5b734cb33159bb6d8b
Kurtnoise13 <Kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-09-12 21:48:07 +0000

    [JobQueue] : scrollbar fix (#1793456)
    git-svn-id: http://svn.code.sf.net/p/megui/code@207 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/core/gui/JobQueue.Designer.cs
+++ trunk/core/gui/JobQueue.Designer.cs
@@ -236,14 +236,14 @@ namespace MeGUI.core.gui
             this.AbortMenuItem,
             this.LoadMenuItem});
             this.queueContextMenu.Name = "queueContextMenu";
-            this.queueContextMenu.Size = new System.Drawing.Size(145, 92);
+            this.queueContextMenu.Size = new System.Drawing.Size(156, 92);
             this.queueContextMenu.Opened += new System.EventHandler(this.queueContextMenu_Opened);
             // 
             // DeleteMenuItem
             // 
             this.DeleteMenuItem.Name = "DeleteMenuItem";
             this.DeleteMenuItem.ShortcutKeyDisplayString = "";
-            this.DeleteMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.DeleteMenuItem.Size = new System.Drawing.Size(155, 22);
             this.DeleteMenuItem.Text = "&Delete";
             this.DeleteMenuItem.ToolTipText = "Delete this job";
             this.DeleteMenuItem.Click += new System.EventHandler(this.deleteJobButton_Click);
@@ -254,20 +254,20 @@ namespace MeGUI.core.gui
             this.PostponedMenuItem,
             this.WaitingMenuItem});
             this.StatusMenuItem.Name = "StatusMenuItem";
-            this.StatusMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.StatusMenuItem.Size = new System.Drawing.Size(155, 22);
             this.StatusMenuItem.Text = "&Change status";
             // 
             // PostponedMenuItem
             // 
             this.PostponedMenuItem.Name = "PostponedMenuItem";
-            this.PostponedMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.PostponedMenuItem.Size = new System.Drawing.Size(136, 22);
             this.PostponedMenuItem.Text = "&Postponed";
             this.PostponedMenuItem.Click += new System.EventHandler(this.postponeMenuItem_Click);
             // 
             // WaitingMenuItem
             // 
             this.WaitingMenuItem.Name = "WaitingMenuItem";
-            this.WaitingMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.WaitingMenuItem.Size = new System.Drawing.Size(136, 22);
             this.WaitingMenuItem.Text = "&Waiting";
             this.WaitingMenuItem.Click += new System.EventHandler(this.waitingMenuItem_Click);
             // 
@@ -275,7 +275,7 @@ namespace MeGUI.core.gui
             // 
             this.AbortMenuItem.Name = "AbortMenuItem";
             this.AbortMenuItem.ShortcutKeyDisplayString = "";
-            this.AbortMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.AbortMenuItem.Size = new System.Drawing.Size(155, 22);
             this.AbortMenuItem.Text = "&Abort";
             this.AbortMenuItem.ToolTipText = "Abort this job";
             this.AbortMenuItem.Click += new System.EventHandler(this.AbortMenuItem_Click);
@@ -285,7 +285,7 @@ namespace MeGUI.core.gui
             this.LoadMenuItem.Enabled = false;
             this.LoadMenuItem.Name = "LoadMenuItem";
             this.LoadMenuItem.ShortcutKeyDisplayString = "";
-            this.LoadMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.LoadMenuItem.Size = new System.Drawing.Size(155, 22);
             this.LoadMenuItem.Text = "&Load";
             this.LoadMenuItem.ToolTipText = "Load into MeGUI";
             // 
@@ -345,8 +345,8 @@ namespace MeGUI.core.gui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.queueListView);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "JobQueue";
             this.Size = new System.Drawing.Size(692, 542);
             this.queueContextMenu.ResumeLayout(false);
