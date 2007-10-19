c35dc62b817eb9975480abdad540f10e95682d5a
sharx1976 <sharx1976@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-10-19 11:55:17 +0000

    Updated Changelog.txt and Version
    git-svn-id: http://svn.code.sf.net/p/megui/code@276 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/AssemblyInfo.cs
+++ trunk/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.2.6.1026")]
+[assembly: AssemblyVersion("0.2.6.1027")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
