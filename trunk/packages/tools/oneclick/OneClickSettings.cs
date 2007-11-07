49fe08ede58ef764aaae1e68798e4ef0df56aafa
berrinam <berrinam@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-11-07 23:26:45 +0000

    (patch by eamonh) Fix #1827919 (OneClickEncode Settings not saved)
    git-svn-id: http://svn.code.sf.net/p/megui/code@335 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/packages/tools/oneclick/OneClickSettings.cs
+++ trunk/packages/tools/oneclick/OneClickSettings.cs
@@ -24,12 +24,84 @@ namespace MeGUI
         {
             return "OneClick";
         }
-        public string VideoProfileName, AudioProfileName, AvsProfileName;
-		public bool PrerenderVideo, DontEncodeAudio, SignalAR, AutomaticDeinterlacing;
-		public long OutputResolution;
-        public FileSize? Filesize;
-        public FileSize? SplitSize;
-        public string[] ContainerCandidates;
+
+        private string videoProfileName;
+        public string VideoProfileName
+        {
+            get { return videoProfileName; }
+            set { videoProfileName = value; }
+        }
+
+        private string audioProfileName;
+        public string AudioProfileName
+        {
+            get { return audioProfileName; }
+            set { audioProfileName = value; }
+        }
+
+        private string avsProfileName;
+        public string AvsProfileName
+        {
+            get { return avsProfileName; }
+            set { avsProfileName = value; }
+        }
+
+        private bool prerenderVideo;
+        public bool PrerenderVideo
+        {
+            get { return prerenderVideo; }
+            set { prerenderVideo = value; }
+        }
+
+        private bool dontEncodeAudio;
+        public bool DontEncodeAudio
+        {
+            get { return dontEncodeAudio; }
+            set { dontEncodeAudio = value; }
+        }
+
+        private bool signalAR;
+        public bool SignalAR
+        {
+            get { return signalAR; }
+            set { signalAR = value; }
+        }
+
+        private bool automaticDeinterlacing;
+        public bool AutomaticDeinterlacing
+        {
+            get { return automaticDeinterlacing; }
+            set { automaticDeinterlacing = value; }
+        }
+
+        private long outputResolution;
+        public long OutputResolution
+        {
+            get { return outputResolution; }
+            set { outputResolution = value; }
+        }
+
+        private FileSize? filesize;
+        public FileSize? Filesize
+        {
+            get { return filesize; }
+            set { filesize = value; }
+        }
+
+        private FileSize? splitSize;
+        public FileSize? SplitSize
+        {
+            get { return splitSize; }
+            set { splitSize = value; }
+        }
+
+        private string[] containerCandidates;
+        public string[] ContainerCandidates
+        {
+            get { return containerCandidates; }
+            set { containerCandidates = value; }
+        }
+
 
         public GenericSettings baseClone()
         {
