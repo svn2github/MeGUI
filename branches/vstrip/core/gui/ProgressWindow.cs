2c288f08f7cbf3b8d90e69517aca4a8c1d4c71af
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-11-13 08:06:35 +0000

    [Progress Window] use TotalHours only, dude...
    git-svn-id: http://svn.code.sf.net/p/megui/code@364 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- branches/vstrip/core/gui/ProgressWindow.cs
+++ branches/vstrip/core/gui/ProgressWindow.cs
@@ -416,8 +416,8 @@ namespace MeGUI
                 fps.Text = su.ProcessingSpeed ?? "---";
 
                 // Time elapsed 
-                // Now using TotalHours, TotalMinutes, TotalSeconds to avoid 24h+ resets...
-                timeElapsed.Text = string.Format("{0:00}:{1:00}:{2:00}", (int)su.TimeElapsed.TotalHours, su.TimeElapsed.TotalMinutes, su.TimeElapsed.TotalSeconds);
+                // Now we use TotalHours to avoid 24h+ resets...
+                timeElapsed.Text = string.Format("{0:00}:{1:00}:{2:00}", (int)su.TimeElapsed.TotalHours, su.TimeElapsed.Minutes, su.TimeElapsed.Seconds);
 
                 // Estimated time
                 totalTime.Text = Util.ToString(su.EstimatedTime) ?? "---";
