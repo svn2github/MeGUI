f04782c52c13285598a0dea2d516c6cebaf3ef9d
berrinam <berrinam@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2007-11-19 22:21:53 +0000

    Add extra files for dragdrop and dropdown persistence
    git-svn-id: http://svn.code.sf.net/p/megui/code@384 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

---
+++ trunk/megui/core/util/CustomUserSettings.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using MeGUI.core.gui;
+using System.ComponentModel;
+
+namespace MeGUI.core.util
+{
+    class CustomUserSettings : ApplicationSettingsBase
+    {
+        private CustomUserSettings() : base() { }
+
+        private static CustomUserSettings defaultInstance = ((CustomUserSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new CustomUserSettings())));
+
+        public static CustomUserSettings Default
+        {
+            get
+            {
+                return defaultInstance;
+            }
+        }
+
+        [UserScopedSetting]
+        public Dar[] CustomDARs
+        {
+            get
+            {
+                object o = this["CustomDARs"];
+                return (Dar[])this["CustomDARs"];
+            }
+            set
+            {
+                this["CustomDARs"] = value; // new ArrayConverter<Named<Dar>, DarConverter>().ToString(value);
+                object o = this["CustomDARs"];
+            }
+        }
+
+        [UserScopedSetting]
+        public FileSize[] CustomSizes
+        {
+            get
+            {
+                return (FileSize[])this["CustomSizes"];
+            }
+            set
+            {
+                this["CustomSizes"] = value;
+            }
+        }
+
+
+        [UserScopedSetting]
+        public FPS[] CustomFPSs
+        {
+            get
+            {
+                return (FPS[])this["CustomFPSs"];
+            }
+            set
+            {
+                this["CustomFPSs"] = value;
+            }
+        }
+    }
+}
