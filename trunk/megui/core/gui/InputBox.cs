3af2957feb1457f52b8ea09193772c9f5a7b4b83
Mutant_Fruit <Mutant_Fruit@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-03-22 23:34:33 +0000

    Two fixes for the new InputBox
    git-svn-id: http://svn.code.sf.net/p/megui/code@476 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- trunk/megui/core/gui/InputBox.cs
+++ trunk/megui/core/gui/InputBox.cs
@@ -17,12 +17,14 @@ namespace MeGUI.core.gui
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-
+            DialogResult = DialogResult.OK;
+            Close();
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-
+            DialogResult = DialogResult.Cancel;
+            Close();
         }
 
         internal static string Show(string message, string title, string defaultText)
@@ -30,7 +32,7 @@ namespace MeGUI.core.gui
             using (InputBox box = new InputBox())
             {
                 box.lblMessage.Text = message;
-                box.text.Text = message;
+                box.text.Text = defaultText;
                 box.Text = title;
                 if (box.ShowDialog() == DialogResult.OK)
                     return box.text.Text;
