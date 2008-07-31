4cc17c391e99ddc16a979a36cab09d4dc9831a5b
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-07-31 20:09:04 +0000

    added DRC to the script for Wav Sources if specified.
    git-svn-id: http://svn.code.sf.net/p/megui/code@615 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/AviSynthAudioEncoder.cs
+++ megui/trunk/AviSynthAudioEncoder.cs
@@ -564,9 +564,15 @@ new JobProcessorFactory(new ProcessorFactory(init), "AviSynthAudioEncoder");
                                     UInt32 DtsHeader = r.ReadUInt32(); // read a LE int_32
 
                                     if (DtsHeader == 0xE8001FFF)
-                                         script.AppendFormat("NicDtsSource(\"{0}\"){1}", audioJob.Input, Environment.NewLine);
+                                    {
+                                        script.AppendFormat("NicDtsSource(\"{0}\"", audioJob.Input);
+                                        if (audioJob.Settings.AutoGain)
+                                            script.AppendFormat(", DRC=1){0}", Environment.NewLine);
+                                        else
+                                            script.Append(")");
+                                    }
                                     else
-                                         script.AppendFormat("RaWavSource(\"{0}\", 2){1}", audioJob.Input, Environment.NewLine);
+                                        script.AppendFormat("RaWavSource(\"{0}\", 2){1}", audioJob.Input, Environment.NewLine);
                                     break;
                                 case 0x0003:         // IEEE Float
                                 case 0xFFFE:         // WAVE_FORMAT_EXTENSIBLE header
@@ -576,7 +582,11 @@ new JobProcessorFactory(new ProcessorFactory(init), "AviSynthAudioEncoder");
                                     script.AppendFormat("NicMPG123Source(\"{0}\"){1}", audioJob.Input, Environment.NewLine);
                                     break;
                                 case 0x2000:         // AC3
-                                    script.AppendFormat("NicAc3Source(\"{0}\"){1}", audioJob.Input, Environment.NewLine);
+                                    script.AppendFormat("NicAc3Source(\"{0}\"", audioJob.Input);
+                                    if (audioJob.Settings.AutoGain)
+                                        script.AppendFormat(", DRC=1){0}", Environment.NewLine);
+                                    else
+                                        script.Append(")"); 
                                     break;
                                 default:
                                     script.AppendFormat("WavSource(\"{0}\"){1}", audioJob.Input, Environment.NewLine);
