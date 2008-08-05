47d7efb0819f7743ded34e9fd596b6ae69f645df
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-08-05 13:28:57 +0000

    add Short2FullLanguageName(.) function to retrieve the full language name from the 2 char code. Needed for the IFO parser.
    git-svn-id: http://svn.code.sf.net/p/megui/code@629 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/util/LanguageSelectionContainer.cs
+++ megui/trunk/core/util/LanguageSelectionContainer.cs
@@ -177,5 +177,158 @@ namespace MeGUI
             }
             return null;
         }
+
+        ///<summary>
+        ///Convert the 2 char strings to the full language name
+        ///</summary>
+        ///
+        public static string Short2FullLanguageName(string LngCode)
+        {
+            string Language = "";
+            switch (LngCode.ToLower())
+            {
+                case "aa": Language = "Afar"; break;
+                case "ab": Language = "Abkhazian"; break;
+                case "af": Language = "Afrikaans"; break;
+                case "am": Language = "Amharic"; break;
+                case "ar": Language = "Arabic"; break;
+                case "as": Language = "Assamese"; break;
+                case "ay": Language = "Aymara"; break;
+                case "az": Language = "Azerbaijani"; break;
+                case "ba": Language = "Bashkir"; break;
+                case "be": Language = "Byelorussian"; break;
+                case "bg": Language = "Bulgarian"; break;
+                case "bh": Language = "Bihari"; break;
+                case "bi": Language = "Bislama"; break;
+                case "bn": Language = "Bengali; Bangla"; break;
+                case "bo": Language = "Tibetan"; break;
+                case "br": Language = "Breton"; break;
+                case "ca": Language = "Catalan"; break;
+                case "co": Language = "Corsican"; break;
+                case "cs": Language = "Czech"; break;
+                case "cy": Language = "Welsh"; break;
+                case "da": Language = "Danish"; break;
+                case "de": Language = "German"; break;
+                case "dz": Language = "Bhutani"; break;
+                case "el": Language = "Greek"; break;
+                case "en": Language = "English"; break;
+                case "eo": Language = "Esperanto"; break;
+                case "es": Language = "Spanish"; break;
+                case "et": Language = "Estonian"; break;
+                case "eu": Language = "Basque"; break;
+                case "fa": Language = "Persian"; break;
+                case "fi": Language = "Finnish"; break;
+                case "fj": Language = "Fiji"; break;
+                case "fo": Language = "Faroese"; break;
+                case "fr": Language = "French"; break;
+                case "fy": Language = "Frisian"; break;
+                case "ga": Language = "Irish"; break;
+                case "gd": Language = "Scots Gaelic"; break;
+                case "gl": Language = "Galician"; break;
+                case "gn": Language = "Guarani"; break;
+                case "gu": Language = "Gujarati"; break;
+                case "ha": Language = "Hausa"; break;
+                case "he": Language = "Hebrew (formerly iw)"; break;
+                case "hi": Language = "Hindi"; break;
+                case "hr": Language = "Croatian"; break;
+                case "hu": Language = "Hungarian"; break;
+                case "hy": Language = "Armenian"; break;
+                case "ia": Language = "Interlingua"; break;
+                case "id": Language = "Indonesian (formerly in)"; break;
+                case "ie": Language = "Interlingue"; break;
+                case "ik": Language = "Inupiak"; break;
+                case "is": Language = "Icelandic"; break;
+                case "it": Language = "Italian"; break;
+                case "iu": Language = "Inuktitut"; break;
+                case "iw": Language = "Hebrew"; break;
+                case "ja": Language = "Japanese"; break;
+                case "jw": Language = "Javanese"; break;
+                case "ka": Language = "Georgian"; break;
+                case "kk": Language = "Kazakh"; break;
+                case "kl": Language = "Greenlandic"; break;
+                case "km": Language = "Cambodian"; break;
+                case "kn": Language = "Kannada"; break;
+                case "ko": Language = "Korean"; break;
+                case "ks": Language = "Kashmiri"; break;
+                case "ku": Language = "Kurdish"; break;
+                case "ky": Language = "Kirghiz"; break;
+                case "la": Language = "Latin"; break;
+                case "ln": Language = "Lingala"; break;
+                case "lo": Language = "Laothian"; break;
+                case "lt": Language = "Lithuanian"; break;
+                case "lv": Language = "Latvian, Lettish"; break;
+                case "mg": Language = "Malagasy"; break;
+                case "mi": Language = "Maori"; break;
+                case "mk": Language = "Macedonian"; break;
+                case "ml": Language = "Malayalam"; break;
+                case "mn": Language = "Mongolian"; break;
+                case "mo": Language = "Moldavian"; break;
+                case "mr": Language = "Marathi"; break;
+                case "ms": Language = "Malay"; break;
+                case "mt": Language = "Maltese"; break;
+                case "my": Language = "Burmese"; break;
+                case "na": Language = "Nauru"; break;
+                case "ne": Language = "Nepali"; break;
+                case "nl": Language = "Dutch"; break;
+                case "no": Language = "Norwegian"; break;
+                case "oc": Language = "Occitan"; break;
+                case "om": Language = "(Afan) Oromo"; break;
+                case "or": Language = "Oriya"; break;
+                case "pa": Language = "Punjabi"; break;
+                case "pl": Language = "Polish"; break;
+                case "ps": Language = "Pashto, Pushto"; break;
+                case "pt": Language = "Portuguese"; break;
+                case "qu": Language = "Quechua"; break;
+                case "rm": Language = "Rhaeto-Romance"; break;
+                case "rn": Language = "Kirundi"; break;
+                case "ro": Language = "Romanian"; break;
+                case "ru": Language = "Russian"; break;
+                case "rw": Language = "Kinyarwanda"; break;
+                case "sa": Language = "Sanskrit"; break;
+                case "sd": Language = "Sindhi"; break;
+                case "sg": Language = "Sangho"; break;
+                case "sh": Language = "Serbo-Croatian"; break;
+                case "si": Language = "Sinhalese"; break;
+                case "sk": Language = "Slovak"; break;
+                case "sl": Language = "Slovenian"; break;
+                case "sm": Language = "Samoan"; break;
+                case "sn": Language = "Shona"; break;
+                case "so": Language = "Somali"; break;
+                case "sq": Language = "Albanian"; break;
+                case "sr": Language = "Serbian"; break;
+                case "ss": Language = "Siswati"; break;
+                case "st": Language = "Sesotho"; break;
+                case "su": Language = "Sundanese"; break;
+                case "sv": Language = "Swedish"; break;
+                case "sw": Language = "Swahili"; break;
+                case "ta": Language = "Tamil"; break;
+                case "te": Language = "Telugu"; break;
+                case "tg": Language = "Tajik"; break;
+                case "th": Language = "Thai"; break;
+                case "ti": Language = "Tigrinya"; break;
+                case "tk": Language = "Turkmen"; break;
+                case "tl": Language = "Tagalog"; break;
+                case "tn": Language = "Setswana"; break;
+                case "to": Language = "Tonga"; break;
+                case "tr": Language = "Turkish"; break;
+                case "ts": Language = "Tsonga"; break;
+                case "tt": Language = "Tatar"; break;
+                case "tw": Language = "Twi"; break;
+                case "ug": Language = "Uighur"; break;
+                case "uk": Language = "Ukrainian"; break;
+                case "ur": Language = "Urdu"; break;
+                case "uz": Language = "Uzbek"; break;
+                case "vi": Language = "Vietnamese"; break;
+                case "vo": Language = "Volapuk"; break;
+                case "wo": Language = "Wolof"; break;
+                case "xh": Language = "Xhosa"; break;
+                case "yi": Language = "Yiddish (formerly ji)"; break;
+                case "yo": Language = "Yoruba"; break;
+                case "za": Language = "Zhuang"; break;
+                case "zh": Language = "Chinese"; break;
+                case "zu": Language = "Zulu"; break;
+            }
+            return Language;
+        }
 	}
 }
