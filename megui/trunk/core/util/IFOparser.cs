845dccfa1ae890b3ea2d55fb2df9d108a0d3b6bc
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-08-06 12:20:17 +0000

    added IFOparser class. My first working draft. Only use with VobSubTool for the moment.
    git-svn-id: http://svn.code.sf.net/p/megui/code@630 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

---
+++ megui/trunk/core/util/IFOparser.cs
@@ -0,0 +1,95 @@
+﻿// ****************************************************************************
+// 
+// Copyright (C) 2008  Kurtnoise (kurtnoise@free.fr)
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MeGUI.core.util
+{
+    public sealed class IFOparser
+    {
+        public static string[] GetSubtitlesStreamsInfos(string FileName)
+        {
+            byte[] buff = new byte[2];
+            byte s = 0;
+            string[] subdesc = new string[s];
+
+            try
+            {
+                FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                Stream sr = br.BaseStream;
+
+                // go to the substream #1
+                sr.Seek(0x255, SeekOrigin.Begin);
+
+                s = br.ReadByte();
+                if (s > 32)
+                    s = 32; // force the max #. According to the specs 32 is the max value for subtitles streams.
+
+                subdesc = new string[s];
+
+                // go to the Language Code
+                sr.Seek(2, SeekOrigin.Current);
+
+                for (int i = 0; i < s; i++)
+                {
+                    // Presence (1 bit), Coding Mode (1bit), Short Language Code (2bits), Language Extension (1bit), Sub Picture Caption Type (1bit)
+                    br.Read(buff, 0, 2);
+                    string ShortLangCode = String.Format("{0}{1}", (char)buff[0], (char)buff[1]);
+
+                    subdesc[i] = "[" + String.Format("{0:00}", i) + "]  - " + LanguageSelectionContainer.Short2FullLanguageName(ShortLangCode);
+
+                    // Go to Code Extension
+                    sr.Seek(1, SeekOrigin.Current);
+                    buff[0] = br.ReadByte();
+
+                    switch (buff[0] & 0x0F)
+                    {
+                        // from http://dvd.sourceforge.net/dvdinfo/sprm.html
+                        case 1: subdesc[i] += " (Caption/Normal Size Char)"; break;
+                        case 2: subdesc[i] += " (Caption/Large Size Char)"; break;
+                        case 3: subdesc[i] += " (Caption For Children)"; break;
+                        case 5: subdesc[i] += " (Closed Caption/Normal Size Char)"; break;
+                        case 6: subdesc[i] += " (Closed Caption/Large Size Char)"; break;
+                        case 7: subdesc[i] += " (Closed Caption For Children)"; break;
+                        case 9: subdesc[i] += " (Forced Caption)"; break;
+                        case 13: subdesc[i] += " (Director Comments/Normal Size Char)"; break;
+                        case 14: subdesc[i] += " (Director Comments/Large Size Char)"; break;
+                        case 15: subdesc[i] += " (Director Comments for Children)"; break;
+                    }
+
+                    if (buff[0] == 0) buff[0] = 1;
+
+                    // go to the next sub stream
+                    sr.Seek(2, SeekOrigin.Current);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+           return subdesc;
+        }
+    }
+}
