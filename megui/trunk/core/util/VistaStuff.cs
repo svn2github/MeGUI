a2a3f0571e26bbbae657dbce79f73275f2121068
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-08-29 07:58:40 +0000

    [VistaStuff] added GPL header.
    git-svn-id: http://svn.code.sf.net/p/megui/code@663 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/util/VistaStuff.cs
+++ megui/trunk/core/util/VistaStuff.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿// ****************************************************************************
+// 
+// Copyright (C) 2005-2008  Doom9 & al
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ****************************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
@@ -214,7 +234,5 @@ namespace MeGUI.core.util
             FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
             FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000
         }
-
     }
-
 }
