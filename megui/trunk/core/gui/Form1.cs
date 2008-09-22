65f5bb003f5f254f78fb33cae9dfb18f5e1a96e9
sharx1976 <sharx1976@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-09-22 17:08:08 +0000

    [MainForm] Fixed a cosmetic bug (#2120787)
    git-svn-id: http://svn.code.sf.net/p/megui/code@732 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/gui/Form1.cs
+++ megui/trunk/core/gui/Form1.cs
@@ -648,7 +648,7 @@ namespace MeGUI
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Location = global::MeGUI.Properties.Settings.Default.MainFormLocation;
             this.Menu = this.mainMenu1;
-            this.MinimumSize = new System.Drawing.Size(516, 500);
+            this.MinimumSize = new System.Drawing.Size(516, 516);
             this.Name = "MainForm";
             this.Load += new System.EventHandler(this.MeGUI_Load);
             this.Shown += new System.EventHandler(this.MainForm_Shown);
