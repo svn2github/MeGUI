23dbde3fd8840da4879deb6ff68073e358a9e341
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-12-20 10:31:51 +0000

    reflect the last changes to the compile-msbuild batch file.
    git-svn-id: http://svn.code.sf.net/p/megui/code@898 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/compile-msbuild.bat
+++ megui/trunk/compile-msbuild.bat
@@ -4,27 +4,27 @@ copy Bin\x86\Release\*.exe .
 rem make dist with everything
 md Dist\BigDist
 copy *.exe .\Dist\BigDist
-copy *.dll .\Dist\BigDist
+copy .\lib\*.dll .\Dist\BigDist
 copy *.txt .\Dist\BigDist
 rem make minimum distribution for install
 md Dist\MinDist
 copy megui.exe .\Dist\MinDist
 copy gpl.txt .\Dist\MinDist
 copy changelog.txt .\Dist\MinDist
-copy MessageBoxExLib.dll .\Dist\MinDist
-copy ICSharpCode.SharpZipLib.dll .\Dist\MinDist
+copy .\lib\MessageBoxExLib.dll .\Dist\MinDist
+copy .\lib\ICSharpCode.SharpZipLib.dll .\Dist\MinDist
 rem also separate distribution into packages
 md Dist\core
 copy megui.exe .\Dist\core
 copy gpl.txt .\Dist\core
 copy changelog.txt .\Dist\core
 md Dist\libs
-copy MessageBoxExLib.dll .\Dist\libs
-copy ICSharpCode.SharpZipLib.dll .\Dist\libs
-copy MediaInfo.dll .\Dist\libs
-copy MediaInfoWrapper.dll .\Dist\libs
+copy .\lib\MessageBoxExLib.dll .\Dist\libs
+copy .\lib\ICSharpCode.SharpZipLib.dll .\Dist\libs
+copy .\lib\MediaInfo.dll .\Dist\libs
+copy .\lib\MediaInfoWrapper.dll .\Dist\libs
 md Dist\avswrapper
-copy AviSynthWrapper.dll .\Dist\avswrapper
+copy .\lib\AviSynthWrapper.dll .\Dist\avswrapper
 md Dist\Data
 copy .\Data\*.xml .\Dist\Data
 EXIT
