a30e5d22b08e8adda40c4f3e33ba2b37e880b477
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2008-12-22 14:38:53 +0000

    [JobWorkerWindow] force the mode to shutdown the JobWorker form (#2042450).
    git-svn-id: http://svn.code.sf.net/p/megui/code@910 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/gui/JobWorkerWindow.cs
+++ megui/trunk/core/gui/JobWorkerWindow.cs
@@ -497,7 +497,7 @@ namespace MeGUI.core.gui
         /// <returns>true if worker was shut down, false otherwise</returns>
         private bool shutdownWorkerIfJobsCompleted()
         {
-            if (mode != JobWorkerMode.CloseOnLocalListCompleted) return false;
+            mode = JobWorkerMode.CloseOnLocalListCompleted; // to force the enclosure
             foreach (TaggedJob j in localJobs.Values)
                 if (j.Status != JobStatus.DONE)
                     return false;
