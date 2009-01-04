27aa3d429f112680709c6ce39c98e7508b677508
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2009-01-04 10:38:47 +0000

    update copyright date...;-)
    git-svn-id: http://svn.code.sf.net/p/megui/code@942 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/util/IFOparser.cs
+++ megui/trunk/core/util/IFOparser.cs
@@ -1,6 +1,6 @@
 ﻿// ****************************************************************************
 // 
-// Copyright (C) 2008  Kurtnoise (kurtnoise@free.fr)
+// Copyright (C) 2009  Kurtnoise (kurtnoise@free.fr)
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
