393d4a3fb1056ba5ee327efbb9d55cd27ad973ec
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2009-01-14 16:18:35 +0000

    [x264Encoder] restrict --aud command to raw avc output files.
    git-svn-id: http://svn.code.sf.net/p/megui/code@967 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/packages/video/x264/x264Encoder.cs
+++ megui/trunk/packages/video/x264/x264Encoder.cs
@@ -314,7 +314,8 @@ new JobProcessorFactory(new ProcessorFactory(init), "x264Encoder");
                     sb.Append("--cqmfile \"" + xs.QuantizerMatrix + "\" ");
             }
 
-            sb.Append("--aud ");  // recommended by the specs
+            if (output.EndsWith(".264"))
+                sb.Append("--aud ");  // recommended by the specs
             sb.Append("--progress "); // ensure that the progress is shown
             if (xs.NoDCTDecimate)
                 sb.Append("--no-dct-decimate ");
