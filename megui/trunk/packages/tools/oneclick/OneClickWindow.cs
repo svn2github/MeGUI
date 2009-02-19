8e0eb31c4bc230e313af6429e03235c3a2d476bf
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2009-02-19 08:55:47 +0000

    [OneClickWindow] the workaround was a bad idea after more testing & digging...
    git-svn-id: http://svn.code.sf.net/p/megui/code@1032 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/packages/tools/oneclick/OneClickWindow.cs
+++ megui/trunk/packages/tools/oneclick/OneClickWindow.cs
@@ -212,7 +212,6 @@ namespace MeGUI
             List<AudioTrackInfo> audioTracks;
             List<SubtitleInfo> subtitles;
             vUtil.openVideoSource(fileName, out audioTracks, out subtitles, out ar, out maxHorizontalResolution);
-            int nb = VideoUtil.getAudioStreamsNb(fileName);
 
             List<object> trackNames = new List<object>();
             trackNames.Add("None");
@@ -243,18 +242,7 @@ namespace MeGUI
             {
                 audioTrack1.SelectedObject = audioTracks[0];
             }
-
-            // just to ensure that we have audio streams
-            // otherwise select "None" to avoid crashing 
-            if (nb == 0)
-            {
-                audioTrack1.SelectedIndex = 0;
-                audioTrack2.SelectedIndex = 0;
-                MessageBox.Show("MeGUI has detected that your input file [" + Path.GetFileName(fileName) +
-                                "] doesn't seem to contain any audio streams. Audio Selection won't be accessible in this case...", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                audioGroupbox.Enabled = false;
-            }
-    
+  
             horizontalResolution.Maximum = maxHorizontalResolution;
             
             string chapterFile = VideoUtil.getChapterFile(fileName);
