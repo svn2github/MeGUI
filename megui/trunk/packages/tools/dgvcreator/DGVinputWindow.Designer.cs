ca716961d349f23dcde06a6b4a4263c79455e212
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2009-03-02 16:05:43 +0000

    [DGVCreator] fix a typo.
    git-svn-id: http://svn.code.sf.net/p/megui/code@1049 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/packages/tools/dgvcreator/DGVinputWindow.Designer.cs
+++ megui/trunk/packages/tools/dgvcreator/DGVinputWindow.Designer.cs
@@ -1,4 +1,26 @@
-﻿namespace MeGUI
+﻿// ****************************************************************************
+// 
+// Copyright (C) 2005-2009  Doom9 & al
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// 
+// ****************************************************************************
+
+using System.ComponentModel;
+
+namespace MeGUI
 {
     partial class DGVinputWindow
     {
@@ -18,6 +40,17 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        /// <summary>
+        ///  prevents the form from closing if we're still processing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DGVinputWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.processing)
+                e.Cancel = true;
         }
 
         #region Windows Form Designer generated code
