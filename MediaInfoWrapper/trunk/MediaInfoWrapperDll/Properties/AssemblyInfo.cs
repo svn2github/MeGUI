0bd3d965a1b65329bef14ef0572af54eb3705df2
kurtnoise13 <kurtnoise13@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2009-04-18 12:38:37 +0000

    update MediaInfo library and its wrapper to the latest release (0.7.14)
    git-svn-id: http://svn.code.sf.net/p/megui/code@1153 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- MediaInfoWrapper/trunk/MediaInfoWrapperDll/Properties/AssemblyInfo.cs
+++ MediaInfoWrapper/trunk/MediaInfoWrapperDll/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("0.7.13.0")]
-[assembly: AssemblyFileVersion("0.7.13.0")]
+[assembly: AssemblyVersion("0.7.14.0")]
+[assembly: AssemblyFileVersion("0.7.14.0")]
