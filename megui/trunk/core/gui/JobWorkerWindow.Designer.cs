72400b6c15f5ea711c88bf6b9c00bbf86ae7c44c
zathor <zathor@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2011-02-19 18:10:02 +0000

    corrected wiki link. Patch by XhmikosR (#3186982)
    git-svn-id: http://svn.code.sf.net/p/megui/code@1966 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/gui/JobWorkerWindow.Designer.cs
+++ megui/trunk/core/gui/JobWorkerWindow.Designer.cs
@@ -114,7 +114,7 @@ namespace MeGUI.core.gui
             // 
             // helpButton1
             // 
-            helpButton1.ArticleName = "Job Worker Window";
+            helpButton1.ArticleName = "Parallel job execution";
             helpButton1.AutoSize = true;
             helpButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             helpButton1.Location = new System.Drawing.Point(3, 3);
