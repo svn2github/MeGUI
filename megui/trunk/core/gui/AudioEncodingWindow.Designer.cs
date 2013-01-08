a062d7a704573000775c7ab5c45e68d5a2493bb6
zathor <zathor@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2013-01-08 21:42:07 +0000

    [Audio Encoding Window] cosmetics
    git-svn-id: http://svn.code.sf.net/p/megui/code@2273 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/core/gui/AudioEncodingWindow.Designer.cs
+++ megui/trunk/core/gui/AudioEncodingWindow.Designer.cs
@@ -36,12 +36,10 @@ namespace MeGUI.core.gui
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.AutoSize = true;
-            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(313, 143);
+            this.button1.Location = new System.Drawing.Point(302, 155);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.Size = new System.Drawing.Size(60, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Cancel";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,28 +47,29 @@ namespace MeGUI.core.gui
             // audioEncodingTab1
             // 
             this.audioEncodingTab1.AudioContainer = "";
-            this.audioEncodingTab1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.audioEncodingTab1.Location = new System.Drawing.Point(0, 0);
+            this.audioEncodingTab1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.audioEncodingTab1.Location = new System.Drawing.Point(0, 12);
             this.audioEncodingTab1.Name = "audioEncodingTab1";
             this.audioEncodingTab1.QueueButtonText = "Update";
-            this.audioEncodingTab1.Size = new System.Drawing.Size(429, 173);
+            this.audioEncodingTab1.Size = new System.Drawing.Size(431, 173);
             this.audioEncodingTab1.TabIndex = 0;
             // 
             // AudioEncodingWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 176);
+            this.ClientSize = new System.Drawing.Size(431, 185);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.audioEncodingTab1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1000, 214);
             this.MinimumSize = new System.Drawing.Size(437, 214);
             this.Name = "AudioEncodingWindow";
-            this.Text = "AudioEncodingWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Audio Encoding Window";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
