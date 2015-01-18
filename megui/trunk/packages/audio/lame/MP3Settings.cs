6c828b849544da3f6adad73bc63cbebd10a6706d
zathor <zathor@a21b8521-4df1-4ed3-b48c-cff5bc73dcb3>
2015-01-18 14:13:24 +0000

    update copyright year to 2015 (patch by Kurtnoise)
    git-svn-id: http://svn.code.sf.net/p/megui/code@2518 a21b8521-4df1-4ed3-b48c-cff5bc73dcb3

--- megui/trunk/packages/audio/lame/MP3Settings.cs
+++ megui/trunk/packages/audio/lame/MP3Settings.cs
@@ -1,6 +1,6 @@
 // ****************************************************************************
 // 
-// Copyright (C) 2005-2009  Doom9 & al
+// Copyright (C) 2005-2015 Doom9 & al
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
